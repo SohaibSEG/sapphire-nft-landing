cdba7cb44a88c02a8a95ce3c01f7332c2e8f5843
Sohaib Segueni <sohaibsegueni8@gmail.com>
2022-08-09 15:38:42 +0100

    added partners

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -1,21 +1,38 @@
 import BuPrimary from "../buttons/buPrimary";
 import HeroCard from "../cards/HeroCard";
-import "./Hero.css"
+import "./Hero.css";
 import StatPiece from "./StatePiece";
 
-function Hero(props){
-    return (<main className="hero-section">
-        <HeroCard ></HeroCard>
-        <h1>Discover, Sell<br/> & Collect Rare<br/> NFTs</h1>
-        <p className="fnt-poppins hero-txt">Digital marketplace for crypto collections and non-fungible tokens (NFTs)</p>
+function Hero(props) {
+  return (
+    <div>
+      <main className="hero-section">
+        <HeroCard></HeroCard>
+        <h1>
+          Discover, Sell
+          <br /> & Collect Rare
+          <br /> NFTs
+        </h1>
+        <p className="fnt-poppins hero-txt">
+          Digital marketplace for crypto collections and non-fungible tokens
+          (NFTs)
+        </p>
         <BuPrimary text="Discover Now"></BuPrimary>
         <div className="hero-stats">
-            <StatPiece value="35k+" title="Artworks"></StatPiece>
-            <StatPiece value="15k+" title="Auctions"></StatPiece>
-            <StatPiece value="25k+" title="Artists"></StatPiece>
-
+          <StatPiece value="35k+" title="Artworks"></StatPiece>
+          <StatPiece value="15k+" title="Auctions"></StatPiece>
+          <StatPiece value="25k+" title="Artists"></StatPiece>
         </div>
-    </main>);
+      </main>
+      <div className="hero-partners">
+        <img src="./images/partners/coinbase.png" alt="coinbase" />
+        <img src="./images/partners/bitgo.png" alt="bitgo" />
+        <img src="./images/partners/exodus.png" alt="exodus" />
+        <img src="./images/partners/trezor.png" alt="trezor" />
+
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
